80fb6b050bcc0bf6eccd14d96faf61921c9d2d90
nmermigas <nikosmermigas01@gmail.com>
2022-11-17 22:35:04 +0200

    fixed Add

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -16,21 +16,70 @@ const Add = () => {
   });
 
   const navigate = useNavigate();
-  const handleChange = (e) => {
+  const handleChange = async (e) => {
     setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const postBook = async (book1) => {
+    console.log(`postBook. ${JSON.stringify(book1)}`);
+    // e.preventDefault();
+    try {
+      let response = await axios.post("http://localhost:3000/books/", book1);
+      console.log(response);
+    } catch (err) {
+      console.error(err.response.data);
+    }
   };
 
   const handleClick = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:3000/books", book);
+      // var title = book.title;
+      var title = book.title;
+      // var author = book.author;
+      var author = book.author;
+      console.log(title);
+      console.log(author);
+
+      // var isbn = book.isbn;
+      // console.log(title, author);
+      const res = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q="${title}"+inauthor:${author}&maxResults=1`
+      );
+      console.dir(res.data.items[0].volumeInfo);
+      var title_api = res.data.items[0].volumeInfo.title;
+      var author_api = res.data.items[0].volumeInfo.authors[0];
+      var image_api = res.data.items[0].volumeInfo.imageLinks
+        ? res.data.items[0].volumeInfo.imageLinks.thumbnail
+        : null;
+      var category_api = res.data.items[0].volumeInfo.categories
+        ? res.data.items[0].volumeInfo.categories[0]
+        : "Δεν υπάρχει διαθέσιμη κατηγορία.";
+      var isbn_api =
+        res.data.items[0].volumeInfo.industryIdentifiers[0].identifier;
+      // var isbn_api = "5555";
+
+      var newBook = {
+        title: title_api,
+        author: author_api,
+        ISBN: isbn_api,
+        category: category_api,
+        image: image_api,
+      };
+
+      postBook(newBook);
       navigate("/books");
-    } catch (err) {}
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  console.log(book);
+  // console.log(book.title);
+
   return (
-    <div className="form" classonChange={handleChange} Name="form">
+    <div className="form" onChange={handleChange} Name="form">
+      <h2>{JSON.stringify(book.title)}</h2>
+      <h2>{JSON.stringify(book.author)}</h2>
       <h1>Πρόσθεσε ένα νέο βιβλίο</h1>
       <input
         type="text"
@@ -50,6 +99,7 @@ const Add = () => {
         onChange={handleChange}
         name="ISBN"
       />
+
       {/* <input
         type="text"
         placeholder="image"
